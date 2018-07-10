fb5e7c4c78fa928860cf9e532a0e9b8c43135d93
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2018-07-10 09:48:06 -0700

    add parens

--- cc2/reward.py
+++ cc2/reward.py
@@ -1,4 +1,4 @@
 from math import log, ceil
 
 def reward(R, gamma, epsilon=.000001):
-    return sum(R*gamma**step for step in range(ceil(log(epsilon, gamma))))
+    return sum(R*(gamma**step) for step in range(ceil(log(epsilon, gamma))))
