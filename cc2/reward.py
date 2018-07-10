540277caa98504539beb64ba58b211d01aa57644
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2018-07-10 09:45:32 -0700

    make one-liner

--- cc2/reward.py
+++ cc2/reward.py
@@ -1,5 +1,4 @@
 from math import log, ceil
 
 def reward(R, gamma, epsilon=.000001):
-    max_steps = ceil(log(epsilon, gamma))
-    return sum(R*gamma**step for step in range(max_steps))
+    return sum(R*gamma**step for step in range(ceil(log(epsilon, gamma))))
