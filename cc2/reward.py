089bd688cc13f629304620552aa9b6ffd0409a99
Darwin Johnson <schwaaweb@gmail.com>
2018-07-10 12:46:02 -0400

    added Marina's List Comprehension from the CC2 lecture

--- cc2/reward.py
+++ cc2/reward.py
@@ -1,6 +1,12 @@
+'''
 def reward(R, gamma):
         accumulator = 0
         for k in range(230):  # fails at 229
                 accumulator += (( gamma ** k ) * R )
 
-        return accumulator  # TODONE
+         return accumulator  # TODONE
+'''         
+def reward(R, gamma): # Marina's List Comprehension
+        return sum([ (gamma**k) * R for k in range(230) ])
+
+
