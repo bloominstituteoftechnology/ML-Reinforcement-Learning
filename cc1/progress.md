059c9f558c2ac0b8fd2df87b9631445a22e9b86a
Troy Bradley <me@bitcointroy.co>
2018-07-10 22:23:52 -0500

    Got environment set up!

--- cc1/progress.md
+++ cc1/progress.md
@@ -1,6 +1,6 @@
 ### Progress So Far:
 hello-world - check
-docker build - todo
+docker build - check
 exercises - todo
 
 ### Laptop specs:
@@ -10,7 +10,9 @@ CPU - i7-3720QM
 GPU - AMD Radeon with 1 GB dedicated, 3 GB Shared - max 4 GB total
 
 ### Plan:
-Run Ubuntu on VirtualBox
-Install Docker
-???
+Run Ubuntu on VirtualBox - check
+Install Docker - check
+Build Docker - check
+Make Run in Hands On Folder - check
+Complete Projects - todo
 PROFIT :D
--- cc2/test_reward.py
+++ cc2/test_reward.py
@@ -3,7 +3,6 @@ from reward import reward
 
 
 class TestReward(unittest.TestCase):
-
     def test_always_passing_test(self):
         self.assertEqual(3 + 1, 4)
 
@@ -11,13 +10,13 @@ class TestReward(unittest.TestCase):
         self.assertAlmostEqual(reward(-2.1, 0.32), -3.0882352, 3)
 
     def test_reward_2(self):
-	    self.assertAlmostEqual(reward(-0.1, 0.55), -0.222222, 3)
+        self.assertAlmostEqual(reward(-0.1, 0.55), -0.222222, 3)
 
     def test_reward_3(self):
-	    self.assertAlmostEqual(reward(3.2, 0.95), 64.0, 3)
+        self.assertAlmostEqual(reward(3.2, 0.95), 64.0, 3)
 
     def test_reward_4(self):
-	    self.assertAlmostEqual(reward(3.0, 0.79), 14.285714, 3)
+        self.assertAlmostEqual(reward(3.0, 0.79), 14.285714, 3)
 
 
 if __name__ == '__main__':
