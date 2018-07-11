6228b74c443ea3e8c14bb496f68f36c68fd54ccf
John Doe <johndoe@example.com>
2018-07-11 17:08:55 -0500

    CC2 solutions and changed readme type, (in to) -> (into)

--- cc2/reward.py
+++ cc2/reward.py
@@ -1,2 +1,19 @@
 def reward(R, gamma):
-	return -1.0  # TODO
+	# return -1.0  # TODO
+
+	# Method 1
+	# return R / (1 - gamma)
+
+	# Method 2
+	return sum([(gamma**k) * R for k in range(10000)])
+
+	# Method 3
+	# k = 0
+	# coef = gamma**k
+	# total = 0
+	# while coef > 0.000001:
+	# 	total += coef * R
+	# 	k += 1
+	# 	coef = gamma**k
+	#
+	# return total
