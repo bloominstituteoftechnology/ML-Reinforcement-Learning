f8dcd5eeba78befb2e2429fb006d0ba17c298f3a
Lorin Fields <lorin.fields@gmail.com>
2018-07-11 16:39:45 -0700

    testing

--- cc2/reward.py
+++ cc2/reward.py
@@ -1,7 +1,12 @@
-import sys
-import mpmath
-from sympy.mpmath import *
+# import sys
+# import mpmath
+# from sympy.mpmath import *
+
+# def reward(R, gamma):
+# 	rBig = nsum(lambda k: gamma^k * R, [0, inf])
+# 	return rBig # TODO
+
 
 def reward(R, gamma):
-	rBig = nsum(lambda k: gamma^k * R, [0, inf])
+	rBig = sum((gamma**k*R for k in range(100000)))
 	return rBig # TODO
